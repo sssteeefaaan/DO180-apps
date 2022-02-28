e83af8b500cfb22446427de62215de48b9e1bc3d
Student User <student@workstation.lab.example.com>
2022-02-28 16:22:53 -0500

    zzulfd-helloworld-zzulfd-s2i-2.apps.eu46.prod.nextcle.com

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,26 @@
-<?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-?>
+<!DOCTYPE html>
+
+<html lang="en">
+
+  <head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>HTML 5 Boilerplate</title>
+
+    <link rel="stylesheet" href="style.css">
+
+  </head>
+
+  <body>
+
+	<h1 style="text-align: center">Zdravo Softversko!</h1>
+	<?php print "Winter is passing!\n";?>
+
+  </body>
+
+</html>
